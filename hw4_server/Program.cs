25a463cc4d9b1e5047dee3196e4eedbee1d3affa
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-03-19 00:55:45 -0700

    Finished part 1 of tutorial on server

---
+++ hw4_server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hw4_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
